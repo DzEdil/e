606f39fcd440f28a73c4da10680b8d2e1979657f
dEdil <eadzhakypov@gmail.com>
2019-02-04 20:14:03 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+// git init - создание локального репозитория.
+// git status - печатает текущие изменения в проекте.
+// git add . - добавить измениния во всех файлах.
+//git
+    }
+}
